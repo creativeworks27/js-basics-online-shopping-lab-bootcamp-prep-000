af97d240c5d31acb1ec9e54bb84e23637885114a
Roberto Romero <creativeworks27@icloud.com>
2018-09-22 18:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,9 +49,13 @@ for (let item in cart) {
 
 function removeFromCart(item) {
   // write your code here
-let updatedCart = [];
-
-  
+ for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
