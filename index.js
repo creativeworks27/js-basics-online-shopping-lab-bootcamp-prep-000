e5455b5c97635f9ecead5062bf20873810152e0a
Roberto Romero <creativeworks27@icloud.com>
2018-09-22 19:31:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function viewCart() {
   // write your code here
   let itemsInCart = "In your cart, you have ";
   if (cart.length < 0){
-    return ""
+    return "Your shopping cart is empty.";
   }
   for (var i = 0; i < cart.length i++){
     
